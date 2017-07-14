2badfc440b6b5c59d3684bcde62217bbff63b80d
Ivan Cehmajster <icehmajster@gaussx.com>
2017-07-14 12:29:09 +0200

    komit

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {FormGroup, FormControl, Validators, FormArray, FormBuilder} from "@angular/forms";
+import {Observable} from "rxjs";
 
 
 @Component({
@@ -12,7 +13,7 @@ import {FormGroup, FormControl, Validators} from "@angular/forms";
   border-left: 5px solid #42A948; /* green */
 }
 
-.ng-invalid:not(form)  {
+input.ng-invalid  {
   border-left: 5px solid #a94442; /* red */
 }
 `
@@ -22,21 +23,62 @@ import {FormGroup, FormControl, Validators} from "@angular/forms";
 export class DataDrivenComponent {
     myForm: FormGroup;
 
-    constructor(){
+    constructor(private formBuilder: FormBuilder){
+        //
+        // this.myForm = new FormGroup({
+        //
+        // });
         let EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        this.myForm = new FormGroup({
-            'username' : new FormControl('Max', Validators.required),
-            'email' : new FormControl('', [Validators.required, Validators.pattern(EMAIL_REGEXP)]),
-            'password': new FormControl('', Validators.required)
-        });
+        this.myForm = formBuilder.group(
+            {
+                'username' : ['', [Validators.required, this.exampleValidator]],
+                'email' : ['', [Validators.required, Validators.pattern(EMAIL_REGEXP)]],
+                'password': ['', [Validators.required, Validators.minLength(3)]],
+                'pero': formBuilder.array([
+                    ['', Validators.required, this.asyncExampleValidator]
+                ])
+            });
     }
-
+    get hobbies() { return this.myForm.get('pero') as FormArray; }
+    addNewHobbie(){
+        this.hobbies.push(new FormControl('', Validators.required, this.asyncExampleValidator));
+    }
     onSubmit(){
-
     console.log(this.myForm);
-
-
     }
 
 
+    // CUSTOM VALIDATOR
+
+        exampleValidator(control: FormControl):{
+        [s: string]: boolean
+        }{
+        if(control.value === "Example"){ // ako je control.value jednak "Example" onda vraća
+            return {example: true};      // objekt. Ovdje validacija PADA ! Mozemo vratiti i false svejedno je imamo li false ili true
+        }
+        else { return null }             // Kako bi validacija PROSLA objekt mora vratiti NULL !
+        }
+
+
+        // ASINKRONI VALIDATOR
+
+        /* NPR. Provjera jeli neki username zauzet, te se stoga moramo spojiti na neki backend server kako bi to provjerili. Response NECE BIT instant !
+         *  - Asinkroni validator mozemo dodati kao treci argument na "new FormControl() konstruktor"
+          *
+          * */
+
+        asyncExampleValidator(control: FormControl): Promise<any> | Observable<any> {
+            const promise = new Promise<any>(
+                (resolve, reject) => {
+                    setTimeout(() => {
+                        if(control.value === 'Example') {
+                          resolve({'invalid': true});
+                        } else{
+                           resolve(null);
+                        }
+                    }, 1500);
+                 }
+            );
+            return promise;
+        }
 }
